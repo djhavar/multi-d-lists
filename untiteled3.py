f4582fc8b0835cd79df92a96dcb55ee0b63f0a33
djhavar <90192071+djhavar@users.noreply.github.com>
2022-06-02 16:47:56 +0530

    Add files via upload

---
+++ untiteled3.py
@@ -0,0 +1,23 @@
+l=['1','2','3']
+print("list of number:")
+print(l)
+
+ml=[['list of number:'], [' 1 2 3']]
+print(ml)
+
+r=[['a','b','c'],['1','2','3'],['true','true','false']]
+
+for record in r:
+ print(record)
+ 
+n1 = 23.454543552
+n2 = 3.1416
+
+sum = n1 + n2 
+
+print('The sum of {0} and {1} is {2}'. format ( n1, n2, sum))
+
+num1 = input('enter number1 : ')
+num2 = input('enter number2 : ')
+s = float(num1)  + float(num2)
+print('The sum of {0} and {1} is {2}'. format ( num1, num2, s))
